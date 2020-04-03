d7547e82ce005888b770d92ad2858b3e9cac43b0
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2020-04-03 00:27:36 -0300

    login parte 1

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -3,18 +3,20 @@ namespace src\controllers;
 
 use \core\Controller;
 
-class HomeController extends Controller {
+class HomeController extends Controller 
+{   
+    private $loggedUser;
 
-    public function index() {
+    public function __construct()
+    {
+        $this->redirect('/login');
+    }
+
+    public function index() 
+    {
         $this->render('home', ['nome' => 'Bonieky']);
     }
 
-    public function sobre() {
-        $this->render('sobre');
-    }
-
-    public function sobreP($args) {
-        print_r($args);
-    }
+    
 
 }
